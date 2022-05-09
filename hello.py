03bc0d44f6ab5d835206c24adc162eb36a40033e
Ademir Ladeira <ademirladeirajunior@gmail.com>
2022-05-09 15:08:45 -0300

    Add coleta de variavel de ambiente LANG, suportando ingles, portugues e italiano

--- hello.py
+++ hello.py
@@ -1,3 +1,35 @@
 #!/usr/bin/env python3
+"""Hello World Multi Linguas
 
-print("Hello, world!")
+Dependendo da lingua configurada no ambiente o programa exibe a mensagem correspondente
+
+Como usar:
+
+Tenha a variavel LANG devidamente configurada ex:
+
+    export LANG=pt_BR
+
+Execução:
+
+    python3 hello.py
+    ou
+    ./hello.py
+"""
+
+__version__ = "0.0.1"
+__author__ = "Ademir Ladeira"
+__license__ = "Unlicense"
+
+import os
+
+current_language = os.getenv("LANG")[:5]
+
+msg = "Hello, World!"
+
+if current_language == "pt_BR":
+    msg = "Olá, Mundo!"
+elif current_language == "it_IT":
+    msg = "Ciao, Mondo!"
+
+print(msg)
+
